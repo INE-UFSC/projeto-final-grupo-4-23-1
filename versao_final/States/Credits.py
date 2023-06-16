66f2a95b80dda8ff4b2bfa59f50be8df0089d914
Artur Soda <artursodar@gmail.com>
2023-06-16 18:59:53 -0300

    fix bugs credits

--- versao_final/States/Credits.py
+++ versao_final/States/Credits.py
@@ -11,11 +11,23 @@ class Credits(State):
         self.create_button()
 
     def create_button(self):
-        x_pos = self.display_width // 2 - 150
+        x_pos = self.display_width // 2
         y_pos = self.display_height // 2
 
         back = Button(self, 20, 20, 180, 100, "<-- Back", True, self.back)
         self.all_sprites.add(back)
+
+        buttonArthur = Button(self, self.x_pos + 50, self.y_pos - 125, 180, 60, "GITHUB", False, self.gitArthur)
+        self.all_sprites.add(buttonArthur)
+
+        buttonBreno = Button(self, self.x_pos + 50, self.y_pos, 180, 60, "GITHUB", False, self.gitBreno)
+        self.all_sprites.add(buttonBreno)
+
+        buttonLeonardo = Button(self, self.x_pos + 50, self.y_pos + 125, 180, 60, "GITHUB", False, self.gitLeonardo)
+        self.all_sprites.add(buttonLeonardo)
+
+        buttonPedro = Button(self, self.x_pos + 50, self.y_pos + 250, 180, 60, "GITHUB", False, self.gitPedro)
+        self.all_sprites.add(buttonPedro)
 
     def back(self):
         self.get_owner().change_state("MainMenu")
@@ -26,30 +38,10 @@ class Credits(State):
 
         self.text("-=-=CREDITS=-=-", self.x_pos - 300, self.y_pos - 270, 40, 'white')
 
-        # name1 = self.get_all_profiles()[0].name if (len(self.get_all_profiles()) >= 1) else "<None>"
-        # self.text("1. %s" % (name1), self.x_pos - 350, self.y_pos - 125, 40, 'yellow')
-        #
-        # score1 = self.get_all_profiles()[0].max_score if (len(self.get_all_profiles()) >= 1) else ""
-        # self.text("%s" % (score1), self.x_pos + 50, self.y_pos - 125, 40, 'yellow')
-
-        #onde eh o score colocar um botao
-        #nome deixa igual a posição
-
         self.text("Arthur", self.x_pos - 350, self.y_pos - 125, 40, 'yellow')
-        buttonArthur = Button(self, self.x_pos + 50, self.y_pos - 125, 180, 60, "GITHUB", False, self.gitArthur)
-        self.all_sprites.add(buttonArthur)
-
         self.text("Breno", self.x_pos - 350, self.y_pos, 40, 'yellow')
-        buttonBreno = Button(self, self.x_pos + 50, self.y_pos, 180, 60, "GITHUB", False, self.gitBreno)
-        self.all_sprites.add(buttonBreno)
-
         self.text("Leonardo", self.x_pos - 350, self.y_pos + 125, 40, 'yellow')
-        buttonLeonardo = Button(self, self.x_pos + 50, self.y_pos + 125, 180, 60, "GITHUB", False, self.gitLeonardo)
-        self.all_sprites.add(buttonLeonardo)
-
         self.text("Pedro", self.x_pos - 350, self.y_pos + 250, 40, 'yellow')
-        buttonPedro = Button(self, self.x_pos + 50, self.y_pos + 250, 180, 60, "GITHUB", False, self.gitPedro)
-        self.all_sprites.add(buttonPedro)
 
     def handle_update(self):
         pygame.display.update()
